fa369ca2b0363bf547aa0cce595617b24d1ae0f5
Yarkz420 <iwamotoarjun@gmail.com>
2026-07-13 18:18:40 -0400

    test commit started reflection problem

---
+++ Simulation/12. Reflection/Reflection.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+public class Reflection {
+    public static void main(String[] args){
+        BufferedReader r = new BufferedReader(InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(System.out);
+        String test = r.readLine();
+        System.err.println(test);
+    }
+}
